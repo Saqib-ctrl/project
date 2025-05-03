26e7badb39bc5203421a3117724bf4667ed25ffb
Muhmmad Najam Us Saqib <bc210411582mnu@vu.edu.pk>
2025-05-03 09:29:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import Base, engine
-from .routers import candidate_router
+from routers import candidate_router
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
